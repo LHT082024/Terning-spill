b78a4825421b3ee5ae97dc4e7a5b00340ec4810e
LHT082024 <LHT082024@gmail.com>
2024-10-17 09:25:54 +0200

    made the basic logic

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Random random = new Random();
+
+
+int roll1 = random.Next(1, 7);
+int roll2 = random.Next(1, 7);
+int roll3 = random.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll {roll1} + {roll2} + {roll3} = {total}");
+

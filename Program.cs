1ebcf78f83a0cecbcb0a2c47087be887b9ee10d2
LHT082024 <LHT082024@gmail.com>
2024-10-17 09:30:20 +0200

    set win lose conditons

--- Program.cs
+++ Program.cs
@@ -9,3 +9,15 @@ int total = roll1 + roll2 + roll3;
 
 Console.WriteLine($"Dice roll {roll1} + {roll2} + {roll3} = {total}");
 
+if (total < 14)
+{
+    Console.WriteLine("You win :)");
+}
+
+if (total > 15)
+{
+    Console.WriteLine("you lose");
+}
+
+// Console.WriteLine($"Dice roll {roll1} + {roll2} + {roll3} = {total}");
+
